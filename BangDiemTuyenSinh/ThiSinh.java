4f45962ddba53647391fa5ab9cb8bc5da166e1d8
tranhangt <73415658+tranhangt@users.noreply.github.com>
2021-11-28 19:40:10 +0700

    Add files via upload

---
+++ BangDiemTuyenSinh/ThiSinh.java
@@ -0,0 +1,41 @@
+package BangDiemTuyenSinh;
+
+public class ThiSinh{
+    private String ma, ten, trangThai;
+    private float toan, ly, hoa, tong, uuTien, xetTuyen;
+
+    public ThiSinh(String ma, String ten, float toan, float ly, float hoa) {
+        this.ma = ma;
+        this.ten = ten;
+        this.toan = toan * 2;
+        this.ly = ly;
+        this.hoa = hoa;
+        this.tong = this.toan + ly + hoa;
+        this.uuTien = uuTien(ma);
+        this.trangThai = tt();
+        this.xetTuyen = tong + uuTien;
+    }
+
+    private float uuTien(String s){
+        String tmp = s.substring(0, 3);
+        if(tmp.equals("KV1")) return 0.5F;
+        if(tmp.equals("KV2")) return 1;
+        return 2.5F;
+    }
+
+    private String format(float a){
+        if(a == (int) a){
+            return String.format("%.0f", a);
+        }
+        return String.format("%.1f", a);
+    }
+    private String tt(){
+        if(tong + uuTien >= 24) return "TRUNG TUYEN";
+        return "TRUOT";
+    }
+
+    @Override
+    public String toString(){
+        return ma + " " + ten + " " + format(uuTien) + " " + format(tong) + " " + trangThai;
+    }
+}
---
+++ SapXepKetQuaTuyenSinh/Main.java
@@ -0,0 +1,30 @@
+package SapXepKetQuaTuyenSinh;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        List<ThiSinh> ds = new ArrayList<>();
+        while (t-- > 0){
+            ds.add(new ThiSinh(sc.nextLine(), sc.nextLine(), Float.parseFloat(sc.nextLine()), Float.parseFloat(sc.nextLine()), Float.parseFloat(sc.nextLine())));
+        }
+        Collections.sort(ds);
+        for(ThiSinh ts: ds)
+            System.out.println(ts);
+    }
+}
+//KV2A002
+//Hoang Thanh Tuan
+//5
+//6
+//5
+//KV2B123
+//Ly Thi Thu Ha
+//8
+//6.5
+//7
